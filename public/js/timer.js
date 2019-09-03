b26343a958813e4bf21d0344b502ce40061e3ef9
Susanna Lakey <susannalakey@gmail.com>
2019-09-03 15:19:44 -0700

    began styling on body and main page

--- public/js/timer.js
+++ public/js/timer.js
@@ -1,5 +1,5 @@
 
-function Timer (count_from, interval, on_zero){
+function Timer(count_from, interval, on_zero) {
   this.count_from = count_from;
   this.current = count_from;
   this.running = false;
@@ -7,17 +7,17 @@ function Timer (count_from, interval, on_zero){
   this.timer_element = $('.timer_blade_actual').first();
   this.outline_width = $('.timer_blade_outline').first().width();
   this.actual_timer = setInterval(() => {
-    if(!this.running || this.current < 0){
+    if (!this.running || this.current < 0) {
       this.running = false;
       return;
     }
     this.current--;
     let percentage = this.current / this.count_from;
     this.timer_element.width(this.outline_width * percentage);
-  
-    if (this.current < 0){
+
+    if (this.current < 0) {
       this.running = false;
-      if(this.on_zero){
+      if (this.on_zero) {
         this.on_zero();
       }
     }
@@ -25,16 +25,15 @@ function Timer (count_from, interval, on_zero){
 }
 
 
-Timer.prototype.pause = function (){
+Timer.prototype.pause = function () {
   this.running = false;
 }
 
-Timer.prototype.resume = function (){
+Timer.prototype.resume = function () {
   this.running = true;
 }
 
 // Move these two lines to whichever page is using our timer
 var timer = new Timer(60, 1000, () => window.location.pathname = '/timer_zero');
 timer.resume();
-
 
