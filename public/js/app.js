aa8fef0333e86e67f5c019f650babfdabb5e5bf6
Susanna Lakey <susannalakey@gmail.com>
2019-09-03 09:14:02 -0700

    added jquery to toggle lightsaber effects on hamburger menu

---
+++ public/js/app.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+  $('#links').hide()
+  $('#sabers').click(function () {
+    $('#links').toggle();
+  })
+
+  $('#sabers').mouseover(function () {
+    $('#obi-plasma').css({ 'background': 'rgb(255, 255, 255)', 'box-shadow': '0px 0px 12px 6px #0ff' });
+    $('#vader-plasma').css({ 'background': 'rgb(255, 255, 255)', 'box-shadow': '0px 0px 12px 6px rgb(224, 20, 16)' })
+    $('#yoda-plasma').css({ 'background': 'rgb(255, 255, 255)', 'box-shadow': '0px 0px 12px 6px rgb(37, 167, 76)' })
+  })
+  $('#sabers').mouseleave(function () {
+    $('#obi-plasma').css({ 'background': 'rgb(255, 255, 255)', 'box-shadow': '0px 0px 12px 3px rgb(233, 235, 235)' });
+    $('#vader-plasma').css({ 'background': 'rgb(255, 255, 255)', 'box-shadow': '0px 0px 12px 3px rgb(233, 235, 235)' })
+    $('#yoda-plasma').css({ 'background': 'rgb(255, 255, 255)', 'box-shadow': '0px 0px 12px 3px rgb(233, 235, 235)' })
+  })
+
+
+})
