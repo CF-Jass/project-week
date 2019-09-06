fa9e44d6a996a75027c46df67ac0c9932bd01024
Jon Veach <veach09@gmail.com>
2019-09-05 17:45:30 -0700

    Removed question limit and added 3 minute timer constraint

--- public/js/timer.js
+++ public/js/timer.js
@@ -19,9 +19,9 @@ function Timer(count_from, interval, name, on_zero) {
     this.current--;
     localStorage.setItem(name, this.current);
     let percentage = this.current / this.count_from;
-    this.timer_element_el.style.width = (this.outline_width * percentage) + "px";
+    this.timer_element_el.style.width = (this.outline_width * percentage) + 'px';
     if (percentage < 1) {
-      this.timer_element_el.style.transition = "width 1s";
+      this.timer_element_el.style.transition = 'width 1s';
     }
     if (this.current < 0) {
       this.running = false;
@@ -31,7 +31,6 @@ function Timer(count_from, interval, name, on_zero) {
       }
     }
   }
-  
   this.actual_timer = setInterval(() => timer_event(), interval);
   timer_event();
 }
@@ -46,5 +45,9 @@ Timer.prototype.resume = function () {
 }
 
 var timer = new Timer(180, 1000, 'trivia_timer', () => {
-  window.location.pathname = '/scores';
+  $.post('/addScore', {
+    username: window.localStorage.getItem('username'),
+    game: 'trivia',
+    score: window.localStorage.getItem('score')
+  }).done(() => window.location.pathname = '/scores')
 })
